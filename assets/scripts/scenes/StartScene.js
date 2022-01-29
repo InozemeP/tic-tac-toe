643869695754684e0ccfda4e480043becc9e02fb
Haro Isida <ebuovchu@gmail.com>
2022-01-29 13:41:51 +0300

    add some finely fix

--- assets/scripts/scenes/StartScene.js
+++ assets/scripts/scenes/StartScene.js
@@ -12,6 +12,21 @@ class StartScene extends Phaser.Scene {
         this.add.sprite(0, 0, 'bg').setOrigin(0);
     }
     createText() {
+        this.add.text(config.width / 2, 50, 'welcome', {
+            font: '50px Boby',
+            fill: '#FFFFFF'
+        }).setOrigin(0.5);
+
+        this.add.text(config.width / 2, 150, 'to', {
+            font: '50px Boby',
+            fill: '#FFFFFF'
+        }).setOrigin(0.5);
+
+        this.add.text(config.width / 2, 250, 'tic-tac-toe', {
+            font: '50px Boby',
+            fill: '#FFFFFF'
+        }).setOrigin(0.5);
+
         this.add.text(config.width / 2, config.height - 100, 'Tap to start!', {
             font: '50px Boby',
             fill: '#FFFFFF'
