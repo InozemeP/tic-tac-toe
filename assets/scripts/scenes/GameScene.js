569ccccded9cf4fa33a41a8aa48a58eda5ed1737
Haro Isida <ebuovchu@gmail.com>
2022-01-29 13:32:41 +0300

    add some finely fix

--- assets/scripts/scenes/GameScene.js
+++ assets/scripts/scenes/GameScene.js
@@ -10,6 +10,7 @@ class GameScene extends Phaser.Scene {
         this.createBackground();
         this.createRectangles();
         this.createText();
+        this.createChoiceImage();
     };
 
     createBackground() {
@@ -43,19 +44,24 @@ class GameScene extends Phaser.Scene {
     };
 
     clickHandler(gameObject) {
+        if (this.values[gameObject.id] !== 'N') {
+            return;
+        }
 
-        if (this.k % 2 === 0) {
+        if (this.k % 2 === 0)  {
             this.values[gameObject.id] = 'X';
             this.crest = this.add.sprite(gameObject.x,gameObject.y ,'crest').setOrigin(0.5);
             this.k++;
-            this.text.setText('choice: CIRCLE');
+            this.choiceCircle.visible = true;
+            this.choiceCrest.visible = false;
             this.checkWin('CREST');
         }
         else {
             this.values[gameObject.id] = 'O';
             this.circle = this.add.sprite(gameObject.x,gameObject.y ,'circle').setOrigin(0.5);
             this.k++;
-            this.text.setText('choice: CREST');
+            this.choiceCircle.visible = false;
+            this.choiceCrest.visible = true;
             this.checkWin('CIRCLE');
         }
     };
@@ -77,11 +83,15 @@ class GameScene extends Phaser.Scene {
             this.input.off('gameobjectdown');
 
             this.text.setText('');
+            this.choiceCrest.visible = false;
+            this.choiceCircle.visible = false;
 
             this.add.text(600, 520, 'Tap to restart', {
                 font: '60px Boby',
                 fill: '#FFFFFF'
             }).setOrigin(0.5);
+            this.setRestart();
+
 
         } else if(!this.values.find((el) => el === "N" )){
 
@@ -93,20 +103,39 @@ class GameScene extends Phaser.Scene {
             this.input.off('gameobjectdown');
 
             this.text.setText('');
+            this.choiceCrest.visible = false;
+            this.choiceCircle.visible = false;
 
             this.add.text(600, 520, 'Tap to restart', {
                 font: '60px Boby',
                 fill: '#FFFFFF'
             }).setOrigin(0.5);
+            this.setRestart();
         }
     };
 
     createText() {
-         this.text = this.add.text(700, 20, 'choice: CREST', {
+         this.text = this.add.text(700, 30, 'choice:', {
             font: '40px Boby',
             fill: '#FFFFFF'
         });
     };
+
+    setRestart() {
+        this.time.delayedCall(0, () => {
+            this.input.on('pointerdown', () => {
+                this.scene.start('Game');
+            });
+        });
+
+    };
+
+    createChoiceImage() {
+        this.choiceCrest = this.add.sprite(1000, 0, 'crest').setOrigin(0);
+        this.choiceCircle = this.add.sprite(1008, 7, 'circle').setOrigin(0);
+        this.choiceCircle.visible = false;
+    };
+
 
 }
 
