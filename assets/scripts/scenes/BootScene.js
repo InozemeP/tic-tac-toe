4b52ec007fc22ee4dcbb68ef82e721b8a40233a1
Haro Isida <ebuovchu@gmail.com>
2022-01-29 13:33:25 +0300

    add some finely fix

--- assets/scripts/scenes/BootScene.js
+++ assets/scripts/scenes/BootScene.js
@@ -4,13 +4,6 @@ class BootScene extends Phaser.Scene {
     }
     preload() {
         this.load.image('bg', 'assets/sprites/background.png');
-        this.load.image('rс', 'assets/sprites/Reg.png');
-        this.load.image('circle', 'assets/sprites/circle.png');
-        this.load.image('crest', 'assets/sprites/crest.png');
-
-
-
-
     }
     create() {
         this.scene.start('Preload');
--- assets/scripts/scenes/PreloadScene.js
+++ assets/scripts/scenes/PreloadScene.js
@@ -4,6 +4,10 @@ class PreloadScene extends Phaser.Scene {
     }
     preload() {
         this.add.sprite(0, 0, 'bg').setOrigin(0);
+
+        this.load.image('rс', 'assets/sprites/Reg.png');
+        this.load.image('circle', 'assets/sprites/circle.png');
+        this.load.image('crest', 'assets/sprites/crest.png');
     }
     create() {
         this.scene.start('Start');
